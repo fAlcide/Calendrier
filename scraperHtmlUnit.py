281213f458ed20c60dda9acd26a19898ac63f612
fAlcide <alcide.faucheron@etu.uphf.fr>
2022-01-04 11:24:08 +0100

    V3 Stable

---
+++ scraperHtmlUnit.py
@@ -0,0 +1,29 @@
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+import time
+import sys
+
+if(len(sys.argv) > 1):
+    print("Aucun arguement détectéé")
+
+chrome_options = Options()  
+chrome_options.add_argument("--headless")  
+
+browser = webdriver.Chrome('/Users/alcidefaucheron/Documents/Calendrier/chromedriver', chrome_options=chrome_options)
+
+browser.get('https://vtmob.uphf.fr/esup-vtclient-up4/stylesheets/desktop/welcome.xhtml')
+
+identifiant = '//*[@id="username"]'
+mdp = '//*[@id="password"]'
+connexion = '//*[@id="login"]/div[3]/input[6]'
+
+calendrier = '//*[@id="j_id12"]/a[3]/img'
+
+browser.find_element_by_xpath(identifiant).send_keys('afauch3_')
+browser.find_element_by_xpath(mdp).send_keys(str(sys.argv[1]))
+browser.find_element_by_xpath(connexion).click()
+browser.find_element_by_xpath(calendrier).click()
+
+time.sleep(2)
+
+browser.close()
---
+++ script.sh
@@ -0,0 +1,17 @@
+#! bin/bash
+
+if test -f "File/calendar.ics"; then
+    rm -r File/calendar.ics;
+fi
+
+echo "Mot de passe UPF et SFTP (++)";
+
+read -s PASSWORD;
+
+python3 scraperHtmlUnit.py $PASSWORD; 
+
+mv *.ics File/calendar.ics;
+
+lftp sftp://u97148976:$PASSWORD@access781503272.webspace-data.io:/Calendar_UPHF/ -e "put File/calendar.ics; bye" 
+
+echo "Calendrier upload";
